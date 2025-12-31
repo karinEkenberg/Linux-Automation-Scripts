04cac8134312f3d1514e38ef34a1d37098f79801
karin <k.ekenberg.dev@gmail.com>
2025-12-31 19:36:17 +0100

    another one

--- startup_split.sh
+++ startup_split.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# 1. Starta Firefox (nu när inställningarna är fixade räcker detta)
+# Starta programmen
 firefox &
+gnome-terminal &
 
-# 2. Starta terminalen (denna vet vi att systemet hittar)
-gnome-terminal --title="HackerTerminal" &
+# Vänta lite längre för säkerhets skull
+sleep 10
 
-# 3. Vänta ordentligt så att systemet hinner rita upp fönstren
-sleep 6
-
-# 4. Flytta Firefox till höger
+# Flytta fönster baserat på enkla sökord
 wmctrl -r "Firefox" -e 0,960,0,960,1040
-
-# 5. Flytta Terminalen till vänster
-wmctrl -r "HackerTerminal" -e 0,0,0,960,1040
+wmctrl -r "Terminal" -e 0,0,0,960,1040
