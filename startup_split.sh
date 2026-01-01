999b1aac5607f68247dd6de7d573586eb9f54436
karin <k.ekenberg.dev@gmail.com>
2026-01-01 12:21:30 +0100

    finally working, used wrong terminal

--- startup_split.sh
+++ startup_split.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Starta programmen
+/usr/bin/xfce4-terminal &
 firefox &
-gnome-terminal &
 
 # Vänta lite längre för säkerhets skull
-sleep 10
+sleep 3
 
 # Flytta fönster baserat på enkla sökord
-wmctrl -r "Firefox" -e 0,960,0,960,1040
 wmctrl -r "Terminal" -e 0,0,0,960,1040
